72f31837adb07c7c5961550929a47428dcc10266
B1ood31 <bloodymic31@gmail.com>
2026-04-27 17:28:18 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Reaper security here",
         // Activity type number (0 = Playing).
         type: 0, 
       },
